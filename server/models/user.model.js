fd35ec7b6502f7e7c10b6fb2a3eee1cd38d2b839
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-17 21:35:48 +0530

    server setup and create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    // password:{
+    //     type:String,
+    //     required:true
+    // },
+    // role:{
+    //     type:String,
+    //     enum:["instructor", "student"],
+    //     default:'student'
+    // },
+    // enrolledCourses:[
+    //     {
+    //         type:mongoose.Schema.Types.ObjectId,
+    //         ref:'Course'
+    //     }
+    // ],
+    // photoUrl:{
+    //     type:String,
+    //     default:""
+    // }
+},{timestamps:true});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User
