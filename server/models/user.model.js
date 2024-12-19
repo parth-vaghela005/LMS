3c393199a52a9fbeef8cd66a1d6803143debc843
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-19 21:29:05 +0530

    update user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true,
     },
-    role:{
-        type:String,
-        enum:["instructor", "student"],
-        default:'student'
+    role: {
+        type: String,
+        enum: ["instructor", "student"],
+        default: 'student',
     },
-    enrolledCourses:[
+    enrolledCourses: [
         {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'Course'
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Course',
         }
     ],
-    photoUrl:{
-        type:String,
-        default:""
+    photoUrl: {
+        type: String,
+        default: "",
     }
-},{timestamps:true});
+}, { timestamps: true });
 
-const User = mongoose.model("User", userSchema);
-module.exports = User
+const User = mongoose.model('User', userSchema);
+module.exports = User;
