90fc5f41da640565071ad38e9fb7793a2dc32880
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-19 21:30:55 +0530

    add routing and toaster

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 
 import Login from "./pages/Login"
+import Navbar from './components/ui/Navbar'
 export default function App() {
   return (
     <>
+    <Navbar/>
     <Login/>
-    {/* < Create /> */}
     </>
   )
 }
