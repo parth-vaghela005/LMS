dc227c5a26ce2c24cf17d20c1f9505da6e91ea29
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-20 21:08:45 +0530

    add router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,44 @@
 
 import Login from "./pages/Login"
-import Navbar from './components/ui/Navbar'
+// import Navbar from './components/ui/Navbar'
+import HeroSection from "./pages/student/HeroSection"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+// import Course from "./pages/student/Course";
+import Courses from "./pages/student/Courses";
+// import <Courses></Courses> from "./pages/student/Courses";
 export default function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <>
+              <HeroSection />
+              <Courses/>
+           
+            </>
+          ),
+        },
+        {
+          path: "login",
+          element: (
+          
+              <Login />
+           
+          ),
+        },
+      ],
+    },
+  ]);
+  
+  
   return (
-    <>
-    <Navbar/>
-    <Login/>
-    </>
+    <main>
+    <RouterProvider router={appRouter} />
+    </main>
   )
 }
