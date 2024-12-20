038b68bcbc9ae342677d5858692e6d37c26f99d5
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-20 21:09:54 +0530

    update main file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,14 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import appStore from './app/store'
 import { Toaster } from './components/ui/sonner'
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={appStore}>
-      <BrowserRouter> 
+      {/* <BrowserRouter>  */}
         <App />
         <Toaster/>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </Provider>
   </StrictMode>,
 )
