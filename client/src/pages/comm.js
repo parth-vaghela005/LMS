8ba8041745c70d5d1012d7718e226c3ae6690454
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-20 21:11:14 +0530

    add comm.js file for lable and placeholder value

--- client/src/pages/comm.js
+++ client/src/pages/comm.js
@@ -1,3 +1,16 @@
-export const  LOGIN = {
-EMAIL:"email"
+const  LOGIN_LBL = {
+email:"Email",
+password:"Password"
 }
+const PLACEHOLDER = {
+    name:"Enter you name",
+    email:"Enter your email",
+    password:"Enter your password"
+
+}
+const  REGiSTRATION_LBL = {
+    name:"Name",
+    email:"Email",
+    password:"Password"
+    }
+    export {LOGIN_LBL,REGiSTRATION_LBL,PLACEHOLDER}
