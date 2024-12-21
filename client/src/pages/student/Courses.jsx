e0277871af567c449f6af3178052ecc3d30d18b0
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-21 21:15:58 +0530

    develop course component

--- client/src/pages/student/Courses.jsx
+++ client/src/pages/student/Courses.jsx
@@ -27,11 +27,12 @@ const Courses = () => {
         <h2 className="font-bold text-3xl text-center mb-10">Our Courses</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {isLoading ? (
-            Array.from({ length: 8 }).map((_, index) => (
+            Array.from({ length: 7 }).map((_, index) => (
               <CourseSkeleton key={index} />
             ))
           ) : (
             arr.map((course, index) => <Course key={index} course={course} />)
+            // <Course />
           )}
         </div>
       </div>
