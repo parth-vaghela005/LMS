991ca1dc25b7777b1fe5543213e85c671235c574
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-23 21:23:41 +0530

    update server file

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const app = express();
 const dotenv = require('dotenv');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
-const User = require('../server/models/user.model.js');
+// const User = require('../server/models/user.model.js');
 const cors = require('cors')
 const authrouter  = require('../server/routes/Authroute.js')
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 dotenv.config()
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
