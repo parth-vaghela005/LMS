ae3a8f8c28440272a97f1bf921349cbdc4e81560
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-23 21:24:45 +0530

    update all

--- server/routes/Authroute.js
+++ server/routes/Authroute.js
@@ -1,6 +1,8 @@
 const express  = require('express')
-const {register,login}  = require('../controller/user.controller.js')
+const {register,login,getUserProfile}  = require('../controller/user.controller.js')
+const isAuthenticated  = require('../middlewares/isAuthenticated.js')
 const router  = express.Router()
 router.post('/registration',register)
 router.post('/login',login)
+router.get('/profile',isAuthenticated,getUserProfile)
 module.exports = router
