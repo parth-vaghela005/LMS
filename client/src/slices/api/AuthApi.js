d195b43674e3da9287c4c41cd26dffcc30d7b3fc
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-23 21:23:28 +0530

    create auth api

--- client/src/slices/api/AuthApi.js
+++ client/src/slices/api/AuthApi.js
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn } from "../AuthSlice.js";
+
 const USER_API = "http://localhost:5000/api/v1/auth/";
+
 export const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({
         baseUrl: USER_API,
-        credentials: "include",
+        credentials: "include", // Ensures cookies are sent with requests
     }),
     endpoints: (builder) => ({
         registerUser: builder.mutation({
@@ -26,7 +28,20 @@ export const authApi = createApi({
                     const result = await queryFulfilled;
                     console.log("Login response: ", result);
                     dispatch(userLoggedIn({ user: result.data.user }));
-                    console.log("Login response: ", result.data.user);
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+        }),
+        loadUser: builder.query({
+            query: () => ({
+                url: "profile",
+                method: "GET",
+            }),
+            async onQueryStarted(_, { queryFulfilled, dispatch }) {
+                try {
+                    const result = await queryFulfilled;
+                    dispatch(userLoggedIn({ user: result.data.user }));
                 } catch (error) {
                     console.log(error);
                 }
@@ -38,4 +53,5 @@ export const authApi = createApi({
 export const {
     useRegisterUserMutation,
     useLoginUserMutation,
+    useLoadUserQuery, // Exporting the loadUser query hook
 } = authApi;
