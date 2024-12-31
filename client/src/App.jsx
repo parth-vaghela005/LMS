86697459a18907a682e75086cac49a89d4f085d0
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-31 22:14:21 +0530

    add routes for lecture and editcourse

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,13 @@ import HeroSection from "./pages/student/HeroSection";
 import Courses from "./pages/student/Courses";
 import MyLearning from "./pages/student/MyLearning";
 import Profile from "./pages/student/Profile";
-// import Sidebar from "./pages/admin/Dashboard";
 import Sidebar from './pages/admin/Sidebar'
 import Dashboard from './pages/admin/Dashboard'
 import CourseTable from './pages/admin/course/CourseTable'
 import AddCourse from './pages/admin/course/AddCourse'
 import EditCourse from "./pages/admin/course/EditCourse";
-
-
+import CreateLecture from "./pages/admin/lecture/CreateLecture";
+import EditLecture from "./pages/admin/lecture/EditLecture";
 export default function App() {
   const appRouter = createBrowserRouter([
     {
@@ -63,6 +62,14 @@ export default function App() {
             {
               path: "course/:courseId",
               element: <EditCourse />,
+            },
+            {
+              path: "course/:courseId/lecture",
+              element: <CreateLecture/>,
+            },
+            {
+              path: "course/:courseId/lecture/:lectureId",
+              element: <EditLecture/>,
             },
           ],
         },
