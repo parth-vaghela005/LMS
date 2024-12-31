a2867c6e715f5b9a9e43654fd733dc4ec9b73ef9
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-12-31 22:16:25 +0530

    create controller for add lecture

--- server/controller/course.controller.js
+++ server/controller/course.controller.js
@@ -1,4 +1,5 @@
 const Course = require("../models/course.model.js");
+const Lecture = require("../models/lecture.model.js");
 const { deleteMediaFromCloudinary, deleteVideoFromCloudinary, uploadMedia } = require("../utils/cloudinary.js");
 const createCourse = async (req, res) => {
     try {
@@ -69,7 +70,7 @@ const editCourse = async (req, res) => {
         if (thumbnail) {
             if (course.courseThumbnail) {
                 const publicId = course.courseThumbnail.split("/").pop().split(".")[0];
-                await deleteMediaFromCloudinary(publicId); 
+                await deleteMediaFromCloudinary(publicId);
             }
             courseThumbnail = await uploadMedia(thumbnail.path);
         }
@@ -119,9 +120,63 @@ const getCourseById = async (req, res) => {
         });
     }
 };
+const createLecture = async (req, res) => {
+    try {
+        const { lectureTitle } = req.body;
+        const { courseId } = req.params;
+
+        if (!lectureTitle || !courseId) {
+            return res.status(400).json({
+                message: "Lecture title is required"
+            })
+        };
+
+        // create lecture
+        const lecture = await Lecture.create({ lectureTitle });
+
+        const course = await Course.findById(courseId);
+        if (course) {
+            course.lectures.push(lecture._id);
+            await course.save();
+        }
+
+        return res.status(201).json({
+            lecture,
+            message: "Lecture created successfully."
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Failed to create lecture"
+        })
+    }
+}
+const getCourseLecture = async (req, res) => {
+    try {
+        const { courseId } = req.params;
+        const course = await Course.findById(courseId).populate("lectures");
+        if (!course) {
+            return res.status(404).json({
+                message: "Course not found"
+            })
+        }
+        return res.status(200).json({
+            lectures: course.lectures
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Failed to get lectures"
+        })
+    }
+}
 module.exports = {
     createCourse,
     getCreatorCourses,
     editCourse,
     getCourseById,
+    createLecture,
+    getCourseLecture
 };
