d1638c2661a26889c7ac35939f60bd26d36bc29f
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-01-01 11:47:10 +0530

    complete

--- server/controller/course.controller.js
+++ server/controller/course.controller.js
@@ -46,7 +46,6 @@ const getCreatorCourses = async (req, res) => {
         });
     }
 };
-
 const editCourse = async (req, res) => {
     try {
         const courseId = req.params.courseId;
@@ -172,11 +171,46 @@ const getCourseLecture = async (req, res) => {
         })
     }
 }
+ const editLecture = async (req,res) => {
+    try {
+        const {lectureTitle, videoInfo, isPreviewFree} = req.body;
+        
+        const {courseId, lectureId} = req.params;
+        const lecture = await Lecture.findById(lectureId);
+        if(!lecture){
+            return res.status(404).json({
+                message:"Lecture not found!"
+            })
+        }
+
+        // update lecture
+        if(lectureTitle) lecture.lectureTitle = lectureTitle;
+        if(videoInfo?.videoUrl) lecture.videoUrl = videoInfo.videoUrl;
+        if(videoInfo?.publicId) lecture.publicId = videoInfo.publicId;
+        lecture.isPreviewFree = isPreviewFree;
+        await lecture.save();
+        const course = await Course.findById(courseId);
+        if(course && !course.lectures.includes(lecture._id)){
+            course.lectures.push(lecture._id);
+            await course.save();
+        };
+        return res.status(200).json({
+            lecture,
+            message:"Lecture updated successfully."
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to edit lectures"
+        })
+    }
+}
 module.exports = {
     createCourse,
     getCreatorCourses,
     editCourse,
     getCourseById,
     createLecture,
-    getCourseLecture
+    getCourseLecture,
+    editLecture
 };
