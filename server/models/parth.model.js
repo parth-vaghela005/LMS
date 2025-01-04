93fe09e0d0ff3dde192b8596e2cd5079db027b01
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-01-04 21:24:50 +0530

    update

---
+++ server/models/parth.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Define the User schema
+const ParthSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+    minlength: [3, 'Name must be at least 3 characters long']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      'Please provide a valid email address'
+    ]
+  },
+  city: {
+    type: String,
+    required: [true, 'City is required']
+  }
+}, { timestamps: true });
+
+// Export the User model
+module.exports = mongoose.model('Parth', ParthSchema);
