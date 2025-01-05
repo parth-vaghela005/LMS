af69ebb80796b16d84e00d07237a6d508ee6e1ca
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-01-05 21:43:02 +0530

    add HorizontalLoader component for loading animation

---
+++ client/src/pages/admin/lecture/HorizontalLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HorizontalLoader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default HorizontalLoader;
