15323f294051a3b75e1da2a8a9eb15e455c4d0bb
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-01-12 22:27:36 +0530

    add BuyCourseButton component for course purchase functionality

---
+++ client/src/components/BuyCourseButton.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import { Button } from "./ui/button";
+// import { useCreateCheckoutSessionMutation } from "@/features/api/purchaseApi";
+import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import { useCreateCheckoutSessionMutation } from "@/slices/api/courseApi";
+const BuyCourseButton = ({ courseId }) => {
+  const [createCheckoutSession, {data, isLoading, isSuccess, isError, error }] =
+    useCreateCheckoutSessionMutation();
+  const purchaseCourseHandler = async () => {
+    console.log(courseId, "frontend");
+    await createCheckoutSession(courseId);
+  };
+  useEffect(()=>{
+    if(isSuccess){
+       if(data?.url){
+        window.location.href = data.url;
+       }else{
+        toast.error("Invalid response from server.")
+       }
+    } 
+    if(isError){
+      toast.error(error?.data?.message || "Failed to create checkout session")
+    }
+  },[data, isSuccess, isError, error])
+
+  return (
+    <Button
+      disabled={isLoading}
+      onClick={purchaseCourseHandler}
+      className="w-full"
+    >
+      {isLoading ? (
+        <>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Please wait
+        </>
+      ) : (
+        "Purchase Course"
+      )}
+    </Button>
+  );
+};
+
+export default BuyCourseButton;
